011e1c8f28fb3493407b2fe15a2d59fb89492d88
Ian Rouse <ianrouse@smartnano2>
2019-07-11 14:52:04 +0100

    Added default AA parameter file, used if AAs are not specified in the .config file

--- src/ConfigFileReader.h
+++ src/ConfigFileReader.h
@@ -57,6 +57,63 @@ public:
         }
 
         handle.close();
+
+
+      //This is ugly but for testing purposes I want to see if it'll work before worrying about optimising the code.
+
+//test to see if the amino acids are already defined in the config file
+int useConfigAA = 0;
+
+        for (std::size_t i = 0; i < m_keys.size(); ++i) {
+if (m_keys[i] == "amino-acids") {
+std::cout << "found amino acids in config file \n";
+useConfigAA = 1;
+}
+
+}
+
+//if not, we load these in from AAParams.dat
+if(useConfigAA==0){
+handle.open("AAParams.dat");
+
+ if(!handle.is_open()) {
+            std::cerr << "Error: Could not find AA parameter file \n";
+            std::exit(1);
+        }
+
+
+
+        while(std::getline(handle, line)) {
+            if (line.empty() || line[0] == '#') {
+                continue;
+            }
+
+            std::size_t pos = line.find('=');
+
+            if (pos == std::string::npos) // Switch
+            {
+                StringFormat::Strip(line);
+
+                m_switches.emplace_back(line);
+            }
+            else
+            {
+                std::string key   = line.substr(0, pos++);
+                std::string value = line.substr(pos, line.size() - pos);
+
+                StringFormat::Strip(key);
+                StringFormat::Strip(value);
+
+                m_keys.emplace_back(key);
+                m_values.emplace_back(value);
+            }
+        }
+
+        handle.close();
+
+}
+
+
     }
 
     void UpdateConfig(Config& config) {
